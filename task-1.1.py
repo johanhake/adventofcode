6914b145666a632d9d62adb119ec5ce80070389b
Johan Hake <johan.hake@gmail.com>
2022-12-02 16:51:57 +0100

    La til dag 1 og 2

---
+++ task-1.1.py
@@ -0,0 +1,16 @@
+import numpy as np
+with open("input-1.1.txt") as f:
+    
+    sum = 0
+    calories = []
+    for tall in f.readlines():
+        if tall != "\n":
+            sum += int(tall)
+        else:
+            calories.append(sum)
+            sum = 0
+
+calories = np.array(calories)
+print("Elf nr:", calories.argmax()+1, "kalorier:", calories.max())
+print("Topp tre Elfs nr:", calories.argsort()[-3:]+1, "kalorier:", np.sort(calories)[-3:].sum())
+
---
+++ task-2.1.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+"""
+A Rock
+B Paper
+C Scissors
+X Rock 1
+Y Paper 2
+Z Scissors 3
+"""
+
+poeng1 = {
+    "A X\n" : 3+1,
+    "A Y\n" : 6+2,
+    "A Z\n" : 0+3,
+    "B X\n" : 0+1,
+    "B Y\n" : 3+2,
+    "B Z\n" : 6+3,
+    "C X\n" : 6+1,
+    "C Y\n" : 0+2,
+    "C Z\n" : 3+3,
+    }
+
+"""
+A Rock 1
+B Paper 2
+C Scissors 3
+X tap
+Y uavgjort
+Z vinst
+"""
+
+poeng2 = {
+    "A X\n" : 0+3,
+    "A Y\n" : 3+1,
+    "A Z\n" : 6+2,
+    "B X\n" : 0+1,
+    "B Y\n" : 3+2,
+    "B Z\n" : 6+3,
+    "C X\n" : 0+2,
+    "C Y\n" : 3+3,
+    "C Z\n" : 6+1,
+    }
+
+with open("input-2.1.txt") as f:
+    resultat = f.readlines()
+    print("Poeng spill 1:", sum(poeng1[r] for r in resultat))
+    print("Poeng spill 2:", sum(poeng2[r] for r in resultat))
+
