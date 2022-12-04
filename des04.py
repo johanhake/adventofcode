8c927b0f824b7b5d8938334380c9ab2e6c39a34f
Johan Hake <johan.hake@gmail.com>
2022-12-04 19:33:50 +0100

    fikset en liten sak

--- des04.py
+++ des04.py
@@ -1,10 +1,9 @@
 with open("input-4.1.txt") as f:
     antall_full_overlapp = 0
-    antall_ikke_overlapp = 0
+    antall_overlapp = 0
     
     # Går igjennom alle seksjonene
-    linjer = f.readlines()
-    for i, seksjoner in enumerate(linjer):
+    for i, seksjoner in enumerate(f.readlines()):
         # Henter ut de to intervallene
         s1, s2 = seksjoner[:-1].split(",")
         start1, slutt1 = [int(s) for s in s1.split("-")]
@@ -14,8 +13,8 @@ with open("input-4.1.txt") as f:
         antall_full_overlapp += (start1 >= start2 and slutt1 <= slutt2) or \
                                 (start1 <= start2 and slutt1 >= slutt2)
             
-        # Adderer ikke overlapp (True == 1)
-        antall_ikke_overlapp += start1 > slutt2 or start2 > slutt1
+        # Adderer ikke (ikke overlapp) (True == 1)
+        antall_overlapp += not (start1 > slutt2 or start2 > slutt1)
             
     print("Antall som har full overlapp:", antall_full_overlapp)
-    print("Antall som har overlapp:", len(linjer)-antall_ikke_overlapp)
+    print("Antall som har overlapp:", antall_overlapp)
